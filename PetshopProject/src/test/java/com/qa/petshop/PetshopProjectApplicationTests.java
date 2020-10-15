ac12b2e0ddec23e68658e994a9daaf15340d9218
Scottynic112 <71728095+Scottynic112@users.noreply.github.com>
2020-10-15 17:21:01 +0100

    Application Test Working

---
+++ PetshopProject/src/test/java/com/qa/petshop/PetshopProjectApplicationTests.java
@@ -0,0 +1,22 @@
+package com.qa.petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.qa.petshop.model.Animal;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+
+@SpringBootTest
+class PetshopProjectApplicationTests {
+
+	@Test //smoke test
+	void contextLoads() {
+	}
+
+	@Test
+	void giveCoverage() {
+		EqualsVerifier.forClass(Animal.class).usingGetClass().verify();
+	}
+
+}
