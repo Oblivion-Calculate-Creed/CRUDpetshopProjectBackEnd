5f0d1bc6609cbbd196091d7c3d9454b9348379ef
Scottynic112 <71728095+Scottynic112@users.noreply.github.com>
2020-10-16 10:36:46 +0100

    Final Back-End

---
+++ PetshopProject/src/main/java/com/qa/petshop/PetshopProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.petshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshopProjectApplication.class, args);
+	}
+
+}
